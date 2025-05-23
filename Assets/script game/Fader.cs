6f3ceb241f154cd88b1fbd546b60324807425840
lea <meynadierlea34@gmail.com>
2025-05-23 16:40:50 +0200

    terminerrrrr

--- Assets/script game/Fader.cs
+++ Assets/script game/Fader.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Fader : MonoBehaviour
 {
@@ -12,10 +13,18 @@ public class Fader : MonoBehaviour
     {
         if (CompareTag("Player"))
             Canva.SetActive(true);
-            player.SetActive(false);
-            _animator.SetTrigger("Fade");
+        player.GetComponent<Rigidbody2D>().isKinematic = true;
+        player.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        player.GetComponent<Renderer>().enabled = false;
+        player.GetComponent<PlayerMovement>().enabled = false;
+        _animator.SetTrigger("Fade");
 
     }
 
     private void OnTriggerExit2D(Collider2D collision) => Canva.SetActive(true);
+    
+    public void jaaj()
+    {
+        SceneManager.LoadScene("Credits");
+    }
 }
